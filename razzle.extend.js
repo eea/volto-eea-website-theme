1b395a49c5199f62e42b2cec84977985d0951eaf
nileshgulia1 <nileshgulia@gmail.com>
2022-02-25 14:17:22 +0530

    make it ready for development

--- razzle.extend.js
+++ razzle.extend.js
@@ -8,6 +8,11 @@ const modify = (config, { target, dev }, webpack) => {
   config.resolve.alias['../../theme.config'] = themeConfigPath;
   const projectRootPath = path.resolve('.');
   const themeLessPath = `${projectRootPath}/node_modules/@eeacms/volto-eea-design-system/theme`;
+
+  config.resolve.alias['eea-design-system-theme'] = dev
+    ? `${projectRootPath}/src/addons/volto-eea-design-system/theme/themes/eea`
+    : `${themeLessPath}/themes/eea`;
+
   const semanticLessPath = `${projectRootPath}/node_modules/semantic-ui-less`;
   const hasDesignSystemInstalled = config.resolve.alias['eea-volto-themes'];
   config.resolve.alias['eea-volto-theme-folder'] = hasDesignSystemInstalled
