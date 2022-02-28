3752d966fec094a22e93d6c7dc5e70a696796304
nileshgulia1 <nileshgulia@gmail.com>
2022-02-28 22:27:45 +0530

    don't use breadcrumbs for now

--- src/customizations/volto/components/theme/Breadcrumbs/Breadcrumbs.jsx
+++ src/customizations/volto/components/theme/Breadcrumbs/Breadcrumbs.jsx
@@ -5,13 +5,10 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import EEABreadcrumbs from '@eeacms/volto-eea-design-system/ui/Breadcrumbs/Breadcrumbs.jsx';
+//import EEABreadcrumbs from '@eeacms/volto-eea-design-system/ui/Breadcrumbs/Breadcrumbs.jsx';
 
-/**
- * Breadcrumbs container class.
- */
 const BreadcrumbsComponent = ({ pathname }) => {
-  return <EEABreadcrumbs pathname={pathname} />;
+  return null;
 };
 
 export default connect((state) => ({
