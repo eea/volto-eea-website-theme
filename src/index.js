41582a39f8e84cd3ef62d9ef302798627e8350f1
Alin Voinea <contact@avoinea.com>
2022-06-13 23:23:14 +0300

    Fix HomepageView typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import installCustomTitle from '@eeacms/volto-eea-website-theme/components/manag
 import CustomCSS from '@eeacms/volto-eea-website-theme/components/theme/CustomCSS/CustomCSS';
 import DraftBackground from '@eeacms/volto-eea-website-theme/components/theme/DraftBackground/DraftBackground';
 import { TokenWidget } from '@eeacms/volto-eea-website-theme/components/theme/Widgets/TokenWidget';
-import HomePageView from '@eeacms/volto-eea-website-theme/components/theme/Homepage/HomepageView';
+import HomePageView from '@eeacms/volto-eea-website-theme/components/theme/Homepage/HomePageView';
 import HomePageInverseView from '@eeacms/volto-eea-website-theme/components/theme/Homepage/HomePageInverseView';
 
 const applyConfig = (config) => {
