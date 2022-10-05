98aba2a17bc876eedbe0547de12eb99024c5e2f7
Alin Voinea <contact@avoinea.com>
2022-10-05 19:43:21 +0300

    feat(StyleWrapper): Re-enable textAlign and fontWeight - refs #148213

--- src/customizations/@eeacms/volto-block-style/StyleWrapper/schema.js
+++ src/customizations/@eeacms/volto-block-style/StyleWrapper/schema.js
@@ -12,11 +12,31 @@ export const StyleSchema = () => {
         title: 'Preset styles',
         fields: ['style_name'],
       },
+      {
+        id: 'text',
+        title: 'Text',
+        fields: ['textAlign', 'fontWeight'],
+      },
     ],
     properties: {
       style_name: {
         title: 'Style',
         widget: 'style_select',
+      },
+      textAlign: {
+        title: 'Text align',
+        widget: 'style_text_align',
+      },
+      fontWeight: {
+        title: 'Font weight',
+        description: 'The weight (or boldness) of the font',
+        choices: [
+          ['300', 'Light'],
+          ['400', 'Regular'],
+          ['500', 'Medium'],
+          ['600', 'SemiBold'],
+          ['700', 'Bold'],
+        ],
       },
     },
     required: [],
