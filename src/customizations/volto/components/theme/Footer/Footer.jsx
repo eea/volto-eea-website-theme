2560a5f57262999b1aa0bd3fa949cc33dd85bd31
Alin Voinea <contact@avoinea.com>
2022-11-18 18:12:46 +0200

    feat(footer): Get footer, copyright, social and contact actions from backend - refs #151856

--- src/customizations/volto/components/theme/Footer/Footer.jsx
+++ src/customizations/volto/components/theme/Footer/Footer.jsx
@@ -4,20 +4,75 @@
  */
 
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+import { flattenToAppURL } from '@plone/volto/helpers';
 import EEAFooter from '@eeacms/volto-eea-design-system/ui/Footer/Footer';
 import config from '@plone/volto/registry';
 
-const Footer = (props) => {
+const Footer = () => {
   const { eea } = config.settings;
+  const {
+    footerActions = [],
+    copyrightActions = [],
+    socialActions = [],
+    contactActions = [],
+  } = useSelector(
+    (state) => ({
+      footerActions: state.actions?.actions?.footer_actions,
+      copyrightActions: state.actions?.actions?.copyright_actions,
+      socialActions: state.actions?.actions?.social_actions,
+      contactActions: state.actions?.actions?.contact_actions,
+    }),
+    shallowEqual,
+  );
+  // ZMI > portal_actions > footer_actions
+  const actions = footerActions.length
+    ? footerActions.map((action) => ({
+        title: action.title,
+        link: flattenToAppURL(action.url),
+      }))
+    : eea.footerOpts.actions;
+
+  // ZMI > portal_actions > copyright_actions
+  const copyright = copyrightActions.length
+    ? copyrightActions.map((action) => ({
+        title: action.title,
+        site: action.title,
+        link: flattenToAppURL(action.url),
+      }))
+    : eea.footerOpts.copyright;
+
+  // ZMI > portal_actions > social_actions
+  const social = socialActions.length
+    ? socialActions.map((action) => ({
+        name: action.id,
+        icon: action.icon,
+        link: action.url,
+      }))
+    : eea.footerOpts.social;
+
+  // ZMI > portal_actions > contact_actions
+  const contacts = contactActions.length
+    ? contactActions.map((action) => ({
+        text: action.title,
+        icon: action.icon,
+        link: flattenToAppURL(action.url),
+      }))
+    : eea.footerOpts.contacts;
+
+  // Update options with actions from backend
+  const options = {
+    ...eea.footerOpts,
+    social,
+    contacts,
+  };
+
   return (
     <EEAFooter>
-      <EEAFooter.SubFooter {...eea.footerOpts} />
+      <EEAFooter.SubFooter {...options} />
       <EEAFooter.Header>{eea.footerOpts.header}</EEAFooter.Header>
       <EEAFooter.Sites sites={eea.footerOpts.sites} />
-      <EEAFooter.Actions
-        actions={eea.footerOpts.actions}
-        copyright={eea.footerOpts.copyright}
-      />
+      <EEAFooter.Actions actions={actions} copyright={copyright} />
     </EEAFooter>
   );
 };
