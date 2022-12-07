81dbf48815fb27facb4f82c9b764540fdf188b2e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-12-07 17:53:02 +0200

    Better razzle.config

--- razzle.extend.js
+++ razzle.extend.js
@@ -4,8 +4,9 @@ const plugins = (defaultPlugins) => {
 };
 const modify = (config, { target, dev }, webpack) => {
   const themeConfigPath = `${__dirname}/theme/theme.config`;
-  config.resolve.alias['../../theme.config$'] = themeConfigPath;
-  config.resolve.alias['../../theme.config'] = themeConfigPath;
+  const { alias } = config.resolve;
+  alias['../../theme.config$'] = themeConfigPath;
+  alias['../../theme.config'] = themeConfigPath;
 
   const designSystem = '@eeacms/volto-eea-design-system';
   const designSystemPath =
@@ -14,19 +15,15 @@ const modify = (config, { target, dev }, webpack) => {
 
   const themeLessPath = path.resolve(`${designSystemPath}/../theme`);
 
-  config.resolve.alias[
-    'eea-design-system-theme'
-  ] = `${themeLessPath}/themes/eea`;
+  alias['eea-design-system-theme'] = `${themeLessPath}/themes/eea`;
 
   const semanticLessPath = path.dirname(
     require.resolve('semantic-ui-less/package.json'),
   );
-  const hasDesignSystemInstalled = config.resolve.alias['eea-volto-themes'];
-  config.resolve.alias['eea-volto-theme-folder'] = hasDesignSystemInstalled
+
+  alias['eea-volto-theme-folder'] = alias['eea-volto-themes']
     ? themeLessPath
     : semanticLessPath;
-
-  console.log(config.resolve.alias);
 
   return config;
 };
