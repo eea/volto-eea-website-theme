7b262ff776695faba8218f2ceb39404014afd79a
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2022-12-27 13:34:18 +0100

    image copyright

---
+++ src/customizations/volto/components/manage/Blocks/Image/View.jsx
@@ -0,0 +1,119 @@
+/**
+ * View image block.
+ * @module components/manage/Blocks/Image/View
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { UniversalLink } from '@plone/volto/components';
+import { Icon } from 'semantic-ui-react';
+import cx from 'classnames';
+import { withBlockExtensions } from '@plone/volto/helpers';
+import { flattenToAppURL, isInternalURL } from '@plone/volto/helpers';
+import { Copyright } from '@eeacms/volto-eea-design-system/ui';
+/**
+ * View image block class.
+ * @class View
+ * @extends Component
+ */
+export const View = ({ data, detached }) => {
+  const href = data?.href?.[0]?.['@id'] || '';
+  const { copyright, copyrightIcon, copyrightPosition } = data;
+  return (
+    <p
+      className={cx(
+        'block image align',
+        {
+          center: !Boolean(data.align),
+          detached,
+        },
+        data.align,
+      )}
+    >
+      {data.url && (
+        <>
+          {(() => {
+            const image = (
+              <div className="container-image" style={{ position: 'relative' }}>
+                <img
+                  className={cx({
+                    'full-width': data.align === 'full',
+                    large: data.size === 'l',
+                    medium: data.size === 'm',
+                    small: data.size === 's',
+                  })}
+                  src={
+                    isInternalURL(data.url)
+                      ? // Backwards compat in the case that the block is storing the full server URL
+                        (() => {
+                          if (data.size === 'l')
+                            return `${flattenToAppURL(
+                              data.url,
+                            )}/@@images/image`;
+                          if (data.size === 'm')
+                            return `${flattenToAppURL(
+                              data.url,
+                            )}/@@images/image/preview`;
+                          if (data.size === 's')
+                            return `${flattenToAppURL(
+                              data.url,
+                            )}/@@images/image/mini`;
+                          return `${flattenToAppURL(data.url)}/@@images/image`;
+                        })()
+                      : data.url
+                  }
+                  alt={data.alt || ''}
+                  loading="lazy"
+                />
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: '90%',
+                    zIndex: '1000',
+                    width: '100%',
+                    paddingRight: '10px',
+                    paddingLeft: '10px',
+                  }}
+                >
+                  {copyright ? (
+                    <Copyright copyrightPosition={copyrightPosition}>
+                      <Copyright.Icon>
+                        <Icon className={copyrightIcon} />
+                      </Copyright.Icon>
+                      <Copyright.Text>{copyright}</Copyright.Text>
+                    </Copyright>
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </div>
+            );
+            if (href) {
+              return (
+                <UniversalLink
+                  href={href}
+                  openLinkInNewTab={data.openLinkInNewTab}
+                >
+                  {image}
+                </UniversalLink>
+              );
+            } else {
+              return image;
+            }
+          })()}
+        </>
+      )}
+    </p>
+  );
+};
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+View.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default withBlockExtensions(View);
