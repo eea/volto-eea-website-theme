f9cdf1251a7b012fe750223226e80ac0045262ce
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-01-03 22:58:40 +0100

    minimal-setup

--- razzle.extend.js
+++ razzle.extend.js
@@ -3,28 +3,6 @@ const plugins = (defaultPlugins) => {
   return defaultPlugins;
 };
 const modify = (config, { target, dev }, webpack) => {
-  const themeConfigPath = `${__dirname}/theme/theme.config`;
-  const { alias } = config.resolve;
-  alias['../../theme.config$'] = themeConfigPath;
-  alias['../../theme.config'] = themeConfigPath;
-
-  const designSystem = '@eeacms/volto-eea-design-system';
-  const designSystemPath =
-    config.resolve.alias[designSystem] ||
-    path.dirname(require.resolve(designSystem));
-
-  const themeLessPath = path.resolve(`${designSystemPath}/../theme`);
-
-  alias['eea-design-system-theme'] = `${themeLessPath}/themes/eea`;
-
-  const semanticLessPath = path.dirname(
-    require.resolve('semantic-ui-less/package.json'),
-  );
-
-  alias['eea-volto-theme-folder'] = alias['eea-volto-themes']
-    ? themeLessPath
-    : semanticLessPath;
-
   return config;
 };
 
