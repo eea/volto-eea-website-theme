182151aba6f11cccb6e8e67078f22df3ab7dd995
andreiggr <andrei.ggr@gmail.com>
2023-02-03 14:49:07 +0200

    allow render of image after container parent is rendered

--- src/customizations/volto/components/manage/Blocks/Image/View.jsx
+++ src/customizations/volto/components/manage/Blocks/Image/View.jsx
@@ -24,111 +24,127 @@ export const View = ({ data, detached }) => {
   const showCopyrightHovering = copyright?.length > 50;
 
   const [hovering, setHovering] = React.useState(false);
+  const [viewLoaded, setViewLoaded] = React.useState(false);
 
   const showCopyright = data?.size === 'l' || !data.size;
 
+  React.useEffect(() => {
+    // using this method because the image is not loaded
+    // correctly on a fresh render of the page
+    // volto loses the parent container of
+    // the image and the image is thrown randomly in the page
+    // not happening if navigating to the page from routes (not fresh render)
+    setViewLoaded(true);
+  }, []);
+
   return (
-    <p
-      className={cx(
-        'block image align',
-        {
-          center: !Boolean(data.align),
-          detached,
-        },
-        data.align,
-      )}
-    >
-      <div
-        className={`image-block-container ${data?.align ? data?.align : ''}`}
-      >
-        {data.url && (
-          <>
-            {(() => {
-              const image = (
-                <>
-                  <img
-                    className={cx({
-                      'full-width': data.align === 'full',
-                      large: data.size === 'l',
-                      medium: data.size === 'm',
-                      small: data.size === 's',
-                    })}
-                    src={
-                      isInternalURL(data.url)
-                        ? // Backwards compat in the case that the block is storing the full server URL
-                          (() => {
-                            if (data.size === 'l')
-                              return `${flattenToAppURL(
-                                data.url,
-                              )}/@@images/image`;
-                            if (data.size === 'm')
-                              return `${flattenToAppURL(
-                                data.url,
-                              )}/@@images/image/preview`;
-                            if (data.size === 's')
-                              return `${flattenToAppURL(
-                                data.url,
-                              )}/@@images/image/mini`;
-                            return `${flattenToAppURL(
-                              data.url,
-                            )}/@@images/image`;
-                          })()
-                        : data.url
-                    }
-                    alt={data.alt || ''}
-                    loading="lazy"
-                  />
-                  <div
-                    onMouseEnter={() =>
-                      showCopyrightHovering ? setHovering(true) : ''
-                    }
-                    onMouseLeave={() =>
-                      showCopyrightHovering ? setHovering(false) : ''
-                    }
-                    className={`copyright-image ${
-                      copyrightPosition ? copyrightPosition : 'left'
-                    }`}
-                  >
-                    {copyright && showCopyright ? (
-                      <Copyright copyrightPosition={copyrightPosition}>
-                        <Copyright.Icon>
-                          <Icon className={copyrightIcon} />
-                        </Copyright.Icon>
-                        {showCopyrightHovering ? (
-                          <>
-                            {hovering && (
-                              <div className="copyright-hover-container">
-                                <Copyright.Text>{copyright}</Copyright.Text>
-                              </div>
+    <>
+      {viewLoaded && (
+        <p
+          className={cx(
+            'block image align',
+            {
+              center: !Boolean(data.align),
+              detached,
+            },
+            data.align,
+          )}
+        >
+          <div
+            className={`image-block-container ${
+              data?.align ? data?.align : ''
+            }`}
+          >
+            {data.url && (
+              <>
+                {(() => {
+                  const image = (
+                    <>
+                      <img
+                        className={cx({
+                          'full-width': data.align === 'full',
+                          large: data.size === 'l',
+                          medium: data.size === 'm',
+                          small: data.size === 's',
+                        })}
+                        src={
+                          isInternalURL(data.url)
+                            ? // Backwards compat in the case that the block is storing the full server URL
+                              (() => {
+                                if (data.size === 'l')
+                                  return `${flattenToAppURL(
+                                    data.url,
+                                  )}/@@images/image`;
+                                if (data.size === 'm')
+                                  return `${flattenToAppURL(
+                                    data.url,
+                                  )}/@@images/image/preview`;
+                                if (data.size === 's')
+                                  return `${flattenToAppURL(
+                                    data.url,
+                                  )}/@@images/image/mini`;
+                                return `${flattenToAppURL(
+                                  data.url,
+                                )}/@@images/image`;
+                              })()
+                            : data.url
+                        }
+                        alt={data.alt || ''}
+                        loading="lazy"
+                      />
+                      <div
+                        onMouseEnter={() =>
+                          showCopyrightHovering ? setHovering(true) : ''
+                        }
+                        onMouseLeave={() =>
+                          showCopyrightHovering ? setHovering(false) : ''
+                        }
+                        className={`copyright-image ${
+                          copyrightPosition ? copyrightPosition : 'left'
+                        }`}
+                      >
+                        {copyright && showCopyright ? (
+                          <Copyright copyrightPosition={copyrightPosition}>
+                            <Copyright.Icon>
+                              <Icon className={copyrightIcon} />
+                            </Copyright.Icon>
+                            {showCopyrightHovering ? (
+                              <>
+                                {hovering && (
+                                  <div className="copyright-hover-container">
+                                    <Copyright.Text>{copyright}</Copyright.Text>
+                                  </div>
+                                )}
+                              </>
+                            ) : (
+                              <Copyright.Text>{copyright}</Copyright.Text>
                             )}
-                          </>
+                          </Copyright>
                         ) : (
-                          <Copyright.Text>{copyright}</Copyright.Text>
+                          ''
                         )}
-                      </Copyright>
-                    ) : (
-                      ''
-                    )}
-                  </div>
-                </>
-              );
-              if (href) {
-                return (
-                  <UniversalLink
-                    href={href}
-                    openLinkInNewTab={data.openLinkInNewTab}
-                  >
-                    {image}
-                  </UniversalLink>
-                );
-              } else {
-                return image;
-              }
-            })()}
-          </>
-        )}
-      </div>
-    </p>
+                      </div>
+                    </>
+                  );
+                  if (href) {
+                    return (
+                      <UniversalLink
+                        href={href}
+                        openLinkInNewTab={data.openLinkInNewTab}
+                      >
+                        {image}
+                      </UniversalLink>
+                    );
+                  } else {
+                    return image;
+                  }
+                })()}
+              </>
+            )}
+          </div>
+        </p>
+      )}
+    </>
   );
 };
 
--- src/customizations/volto/components/manage/Blocks/LeadImage/View.jsx
+++ src/customizations/volto/components/manage/Blocks/LeadImage/View.jsx
@@ -22,81 +22,92 @@ const View = ({ data, properties }) => {
   const showCopyrightHovering = copyright?.length > 50;
 
   const [hovering, setHovering] = React.useState(false);
+  const [viewLoaded, setViewLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    setViewLoaded(true);
+  }, []);
 
   return (
-    <p
-      className={cx(
-        'block image align',
-        {
-          center: !Boolean(data.align),
-        },
-        data.align,
-      )}
-    >
-      <div
-        className={`image-block-container ${data?.align ? data?.align : ''}`}
-      >
-        {properties.image && (
-          <>
-            {(() => {
-              const image = (
-                <div className="image-block">
-                  <img
-                    className={cx({ 'full-width': data.align === 'full' })}
-                    src={flattenToAppURL(properties.image.download)}
-                    alt={properties.image_caption || ''}
-                  />
-                  <div
-                    onMouseEnter={() =>
-                      showCopyrightHovering ? setHovering(true) : ''
-                    }
-                    onMouseLeave={() =>
-                      showCopyrightHovering ? setHovering(false) : ''
-                    }
-                    className={`copyright-image ${
-                      copyrightPosition ? copyrightPosition : 'left'
-                    }`}
-                  >
-                    {copyright ? (
-                      <Copyright copyrightPosition={copyrightPosition}>
-                        <Copyright.Icon>
-                          <Icon className={copyrightIcon} />
-                        </Copyright.Icon>
-                        {showCopyrightHovering ? (
-                          <>
-                            {hovering && (
-                              <div className="copyright-hover-container">
-                                <Copyright.Text>{copyright}</Copyright.Text>
-                              </div>
+    <>
+      {viewLoaded && (
+        <p
+          className={cx(
+            'block image align',
+            {
+              center: !Boolean(data.align),
+            },
+            data.align,
+          )}
+        >
+          <div
+            className={`image-block-container ${
+              data?.align ? data?.align : ''
+            }`}
+          >
+            {properties.image && (
+              <>
+                {(() => {
+                  const image = (
+                    <div className="image-block">
+                      <img
+                        className={cx({ 'full-width': data.align === 'full' })}
+                        src={flattenToAppURL(properties.image.download)}
+                        alt={properties.image_caption || ''}
+                      />
+                      <div
+                        onMouseEnter={() =>
+                          showCopyrightHovering ? setHovering(true) : ''
+                        }
+                        onMouseLeave={() =>
+                          showCopyrightHovering ? setHovering(false) : ''
+                        }
+                        className={`copyright-image ${
+                          copyrightPosition ? copyrightPosition : 'left'
+                        }`}
+                      >
+                        {copyright ? (
+                          <Copyright copyrightPosition={copyrightPosition}>
+                            <Copyright.Icon>
+                              <Icon className={copyrightIcon} />
+                            </Copyright.Icon>
+                            {showCopyrightHovering ? (
+                              <>
+                                {hovering && (
+                                  <div className="copyright-hover-container">
+                                    <Copyright.Text>{copyright}</Copyright.Text>
+                                  </div>
+                                )}
+                              </>
+                            ) : (
+                              <Copyright.Text>{copyright}</Copyright.Text>
                             )}
-                          </>
+                          </Copyright>
                         ) : (
-                          <Copyright.Text>{copyright}</Copyright.Text>
+                          ''
                         )}
-                      </Copyright>
-                    ) : (
-                      ''
-                    )}
-                  </div>
-                </div>
-              );
-              if (data.href) {
-                return (
-                  <UniversalLink
-                    href={data.href}
-                    openLinkInNewTab={data.openLinkInNewTab}
-                  >
-                    {image}
-                  </UniversalLink>
-                );
-              } else {
-                return image;
-              }
-            })()}
-          </>
-        )}
-      </div>
-    </p>
+                      </div>
+                    </div>
+                  );
+                  if (data.href) {
+                    return (
+                      <UniversalLink
+                        href={data.href}
+                        openLinkInNewTab={data.openLinkInNewTab}
+                      >
+                        {image}
+                      </UniversalLink>
+                    );
+                  } else {
+                    return image;
+                  }
+                })()}
+              </>
+            )}
+          </div>
+        </p>
+      )}
+    </>
   );
 };
 
