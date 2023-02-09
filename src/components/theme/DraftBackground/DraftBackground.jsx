e5d2ce00f865cbe5ddb95030cb8ccd8a609ebf02
Mimi <cretu.mihaelaa11@gmail.com>
2023-02-09 14:36:02 +0200

    feat: show or remove the draft watermark based on the value from the ENV file

--- src/components/theme/DraftBackground/DraftBackground.jsx
+++ src/components/theme/DraftBackground/DraftBackground.jsx
@@ -7,23 +7,34 @@ import { compose } from 'redux';
 
 /**
  * The review_state and id don't change everytime the page is changed and because of that the draft background
- * will apear on pages that shouldn't have it.
+ * will apear on pages that shouldn't have it. The RAZZLE_DISABLE_DRAFT_WATERMARK varible from ENV file should have two possible values:
+ * "Hide-No-Workflow" and "Hide-All". If the variable is not present, it should follow the current logic(show the draft image everywhere),
+ * if the value is "Hide-No-Workflow", then the draft image is not shown on the /login and /controlpanel and if the value is "Hide-All", then
+ * the draft image is not visible at all.
  * For example, if the current page is /datatable (that has the draft background) and then we go to Content Types page,
  * the review_state and id will be the same as the ones from /datatable, so the draft background will still be present. By checking
- * if the pathname from (from withRouter) is different than 'login' or 'controlpanel', we decide if the draft backgound can be present or not.
+ * if the pathname from (from withRouter) is different than 'login' or 'controlpanel' and based on the varible from ENV,
+ * we decide if the draft backgound can be present or not.
  * @param {Object} props
  * @returns
  */
 const DraftBackground = (props) => {
   const draftClass = `wf-state-${props.review_state}`;
-
-  return props.review_state &&
+  const razzleDraft = process.env.RAZZLE_DISABLE_DRAFT_WATERMARK || 'default';
+  const isReviewableStateComponent =
+    props.review_state &&
     !props.pathname.match('login') &&
-    !props.pathname.match('controlpanel') ? (
-    <BodyClass className={draftClass} />
-  ) : (
-    <BodyClass className="wf-state-published" />
-  );
+    !props.pathname.match('controlpanel');
+
+  const obj = {
+    'Hide-All': 'wf-state-published',
+    'Hide-No-Workflow': isReviewableStateComponent
+      ? draftClass
+      : 'wf-state-published',
+    default: draftClass,
+  };
+
+  return <BodyClass className={obj[razzleDraft]} />;
 };
 
 export default compose(
