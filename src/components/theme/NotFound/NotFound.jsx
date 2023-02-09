446c1cdd121dd80a9e1efac2ee897b050eb99215
nileshgulia1 <nileshgulia@gmail.com>
2023-02-09 15:04:25 +0530

    do not customize 404

--- src/components/theme/NotFound/NotFound.jsx
+++ src/components/theme/NotFound/NotFound.jsx
@@ -1,62 +1,6 @@
-/**
- * Home container.
- * @module components/theme/NotFound/NotFound
- */
-
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
 import { compose } from 'redux';
 import { injectIntl } from 'react-intl';
-import { withServerErrorCode } from '@plone/volto/helpers/Utils/Utils';
 import { withRootNavigation } from '@eeacms/volto-eea-website-theme/hocs';
+import { NotFound } from '@plone/volto/components';
 
-/**
- * Not found function.
- * @function NotFound
- * @returns {string} Markup of the not found page.
- */
-const NotFound = (props) => (
-  <>
-    <Container className="view-wrapper">
-      <h1>
-        <FormattedMessage
-          id="This page does not seem to exist…"
-          defaultMessage="This page does not seem to exist…"
-        />
-      </h1>
-      <p className="description">
-        <FormattedMessage
-          id="We apologize for the inconvenience, but the page you were trying to access is not at this address. You can use the links below to help you find what you are looking for."
-          defaultMessage="We apologize for the inconvenience, but the page you were trying to access is not at this address. You can use the links below to help you find what you are looking for."
-        />
-      </p>
-      <p>
-        <FormattedMessage
-          id="If you are certain you have the correct web address but are encountering an error, please contact the {site_admin}."
-          defaultMessage="If you are certain you have the correct web address but are encountering an error, please contact the {site_admin}."
-          values={{
-            site_admin: (
-              <Link to="/contact-form">
-                <FormattedMessage
-                  id="Site Administration"
-                  defaultMessage="Site Administration"
-                />
-              </Link>
-            ),
-          }}
-        />
-      </p>
-      <p>
-        <FormattedMessage id="Thank you." defaultMessage="Thank you." />
-      </p>
-    </Container>
-  </>
-);
-
-export default compose(
-  injectIntl,
-  withRootNavigation,
-  withServerErrorCode(404),
-)(NotFound);
+export default compose(injectIntl, withRootNavigation)(NotFound);
