ea5ae0445ae9f108a9493d755ec626aeea27127a
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-04-05 10:38:30 +0300

    remove razzle env

--- src/components/theme/DraftBackground/DraftBackground.jsx
+++ src/components/theme/DraftBackground/DraftBackground.jsx
@@ -4,50 +4,24 @@ import './draft.css';
 import { BodyClass } from '@plone/volto/helpers';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
-import { runtimeConfig } from '@plone/volto/runtime_config';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
-const removeTrailingSlash = (str) => {
-  return str.replace(/\/+$/, '');
-};
-
 /**
- * The review_state and id don't change everytime the page is changed and because of that the draft background
- * will apear on pages that shouldn't have it. The RAZZLE_DISABLE_DRAFT_WATERMARK varible from ENV file should have two possible values:
- * "Hide-No-Workflow" and "Hide-All". If the variable is not present, it should follow the current logic(show the draft image everywhere),
- * if the value is "Hide-No-Workflow", then the draft image is not shown on pages like login or controlpanel and if the value is "Hide-All", then
- * the draft image is not visible at all.
- * For example, if the current page is /datatable (that has the draft background) and then we go to Content Types page,
- * the review_state and id will be the same as the ones from /datatable, so the draft background will still be present. By checking
- * if the pathname from (from withRouter) is different than the one from state.content.data and based on the varible from ENV,
- * we decide if the draft backgound can be present or not.
  * @param {Object} props
  * @returns
  */
 const DraftBackground = (props) => {
-  let draftClass = 'wf-state-';
+  let draftClass = 'wf-state-is-draft';
   if (
-    props.review_state === null &&
-    props.content.parent.review_state === 'published'
+    (props?.review_state === null &&
+      props?.content?.parent?.review_state === 'published') ||
+    props?.review_state === 'published' ||
+    (props?.review_state === null &&
+      Object.keys(props?.content?.parent || {}).length === 0)
   ) {
-    draftClass += 'published';
-  } else draftClass += `${props.review_state}`;
-
-  const razzleDraft =
-    runtimeConfig['RAZZLE_DISABLE_DRAFT_WATERMARK'] || 'default';
-  const isReviewableStateComponent =
-    props.review_state &&
-    props.contentId === removeTrailingSlash(props.pathname);
-
-  const draftOptions = {
-    'Hide-All': 'wf-state-published',
-    'Hide-No-Workflow': isReviewableStateComponent
-      ? draftClass
-      : 'wf-state-published',
-    default: draftClass,
-  };
-
-  return <BodyClass className={draftOptions[razzleDraft]} />;
+    draftClass = '';
+  }
+  return draftClass ? <BodyClass className={draftClass} /> : '';
 };
 
 export default compose(
