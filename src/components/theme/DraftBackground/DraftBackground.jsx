ddcc64c1e1198113c832bb875a795c0b7d15b029
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-04-13 12:22:38 +0300

    fix login page, root page

--- src/components/theme/DraftBackground/DraftBackground.jsx
+++ src/components/theme/DraftBackground/DraftBackground.jsx
@@ -14,16 +14,40 @@ import { flattenToAppURL } from '@plone/volto/helpers';
 const checkIfNullOrUndefined = (value) => {
   return value === undefined || value === null;
 };
+const removeTrailingSlash = (str) => {
+  return str.replace(/\/+$/, '');
+};
 
+const checkIfPublished = (props) => {
+  console.log({ props }, removeTrailingSlash(props.pathname));
+  //case 0: the state is not for the current content-type
+  if (props.contentId !== removeTrailingSlash(props.pathname)) return true;
+
+  //case 1 : review_state published
+  if (props?.review_state === 'published') return true;
+  //case 2: review_state null, but parent is published
+  if (
+    checkIfNullOrUndefined(props?.review_state) &&
+    props?.content?.parent?.review_state === 'published'
+  )
+    return true;
+  //case 3: review_state null, but there is no parent
+  if (
+    checkIfNullOrUndefined(props?.review_state) &&
+    Object.keys(props?.content?.parent || {}).length === 0
+  )
+    return true;
+  //case 4: review_state null, and review state of parent is null
+  if (
+    checkIfNullOrUndefined(props?.review_state) &&
+    checkIfNullOrUndefined(props?.content?.parent?.review_state)
+  )
+    return true;
+  return false;
+};
 const DraftBackground = (props) => {
   let draftClass = 'wf-state-is-draft';
-  if (
-    (checkIfNullOrUndefined(props?.review_state) &&
-      props?.content?.parent?.review_state === 'published') ||
-    props?.review_state === 'published' ||
-    (checkIfNullOrUndefined(props?.review_state) &&
-      Object.keys(props?.content?.parent || {}).length === 0)
-  ) {
+  if (checkIfPublished(props)) {
     draftClass = '';
   }
   return draftClass ? <BodyClass className={draftClass} /> : '';
