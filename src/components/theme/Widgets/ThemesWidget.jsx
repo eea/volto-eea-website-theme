036f4711ace0985e2b542c64a1cbe28fd97911bd
Alin Voinea <contact@avoinea.com>
2023-06-16 20:09:35 +0300

    feat: ThemesWidget update advanced-search link - refs #145772

--- src/components/theme/Widgets/ThemesWidget.jsx
+++ src/components/theme/Widgets/ThemesWidget.jsx
@@ -7,7 +7,7 @@ export const ThemesWidget = ({ value, children, className }) => {
     <span className={cx(className, 'token', 'widget')}>
       {value.map((tag) => (
         <Tag
-          href={`https://www.eea.europa.eu/en/advanced-search?filters[0][field]=topic&filters[0][values][0]=${tag.title}&filters[0][type]=any&filters[1][field]=issued.date&filters[1][type]=any&filters[1][values][0]=Last 5 years&filters[2][field]=language&filters[2][type]=any&filters[2][values][0]=en`}
+          href={`https://www.eea.europa.eu/en/advanced-search?filters[0][field]=topic&filters[0][values][0]=${tag.title}&filters[0][type]=any&filters[1][field]=language&filters[1][type]=any&filters[1][values][0]=en&sort-field=issued.date&sort-direction=desc`}
           key={tag.token}
         >
           {children ? children(tag.title) : tag.title}
