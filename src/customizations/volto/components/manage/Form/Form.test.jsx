03201f87a5e6250d6e8dca05f2cf1a704d74a8a8
ana-oprea <anaop3212@gmail.com>
2023-07-17 16:36:16 +0300

    test: add unit tests for Form - refs #254313

--- src/customizations/volto/components/manage/Form/Form.test.jsx
+++ src/customizations/volto/components/manage/Form/Form.test.jsx
@@ -24,6 +24,41 @@ jest.mock('@plone/volto/components/manage/Form/Field', () => (props) => {
   );
 });
 
+jest.mock(
+  '@plone/volto/components/manage/Form/BlocksToolbar',
+  () => (props) => {
+    return (
+      <input
+        id={'blocks-toolbar'}
+        onClick={(target) => {
+          props.onSetSelectedBlocks(
+            target.target.value ? [...target.target.value.split(',')] : [],
+          );
+        }}
+        onChange={props.onChangeBlocks}
+        onSelect={(target) => {
+          props.onSelectBlock(
+            target.target.id,
+            target.target.isMultipleSelection,
+            target.target.event,
+          );
+        }}
+      />
+    );
+  },
+);
+
+jest.mock('@plone/volto/components/manage/Form/UndoToolbar', () => (props) => {
+  return <div>UndoToolbar</div>;
+});
+
+jest.mock(
+  '@plone/volto/components/manage/Blocks/Block/BlocksForm',
+  () => (props) => {
+    return <input id={'blocks-form'} onChange={props.onChangeFormData} />;
+  },
+);
+
 describe('Form', () => {
   beforeEach(() => {
     store = mockStore({
@@ -319,7 +354,7 @@ describe('Form', () => {
     );
   });
 
-  it('renders without crashing and selecting Submit/Cancel button', () => {
+  it('renders without crashing and selecting Submit/Cancel button with resetAfterSubmit and errors', () => {
     config.blocks = {
       initialBlocksFocus: null,
     };
@@ -341,7 +376,193 @@ describe('Form', () => {
             title: 'Fieldset 2',
           },
         ],
-        properties: {},
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { widget: 'textarea' },
+        },
+        definitions: {},
+        required: [],
+      },
+      formData: {
+        blocks: {
+          id1: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+        },
+        blocks_layout: {
+          items: ['id1'],
+        },
+      },
+      type: 'typeB',
+      isClient: true,
+      title: 'Test title',
+      description: 'Test description',
+      error: {
+        message: 'Sample error message',
+      },
+      onSubmit: jest.fn(),
+      onCancel: jest.fn(),
+      resetAfterSubmit: true,
+      submitLabel: 'Submit',
+    };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => ({
+      field1: [],
+      field2: [],
+    }));
+
+    const { container, getByText } = render(
+      <Provider store={store}>
+        <Form {...props} />
+      </Provider>,
+    );
+
+    expect(getByText('Fieldset 1')).toBeInTheDocument();
+    expect(getByText('Fieldset 2')).toBeInTheDocument();
+    expect(getByText('Test title')).toBeInTheDocument();
+    expect(container.querySelector('#mocked-field-field1')).toBeInTheDocument();
+    expect(container.querySelector('#mocked-field-field2')).toBeInTheDocument();
+    expect(
+      container.querySelector('#mocked-field-field3'),
+    ).not.toBeInTheDocument();
+    expect(
+      container.querySelector('#mocked-field-field4'),
+    ).not.toBeInTheDocument();
+
+    fireEvent.click(container.querySelector('button[aria-label="Submit"]'));
+    fireEvent.click(container.querySelector('button[aria-label="Cancel"]'));
+  });
+
+  it('renders without crashing and selecting Submit/Cancel button with resetAfterSubmit and isaEditForm and no errors', () => {
+    config.blocks = {
+      initialBlocksFocus: null,
+    };
+    config.settings = {
+      verticalFormTabs: true,
+    };
+
+    const props = {
+      schema: {
+        fieldsets: [
+          {
+            fields: ['field1', 'field2'],
+            id: 'fieldset1',
+            title: 'Fieldset 1',
+          },
+          {
+            fields: ['field3', 'field4'],
+            id: 'fieldset2',
+            title: 'Fieldset 2',
+          },
+        ],
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { widget: 'textarea' },
+        },
+        definitions: {},
+        required: [],
+      },
+      formData: {
+        blocks: {
+          id1: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+        },
+        blocks_layout: {
+          items: ['id1'],
+        },
+      },
+      type: 'typeB',
+      isClient: true,
+      title: 'Test title',
+      description: 'Test description',
+      error: {
+        message: 'Sample error message',
+      },
+      onSubmit: jest.fn(),
+      onCancel: jest.fn(),
+      resetAfterSubmit: true,
+      isEditForm: true,
+      submitLabel: 'Submit',
+    };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => ({}));
+
+    const { container, getByText } = render(
+      <Provider store={store}>
+        <Form {...props} />
+      </Provider>,
+    );
+
+    expect(getByText('Fieldset 1')).toBeInTheDocument();
+    expect(getByText('Fieldset 2')).toBeInTheDocument();
+    expect(getByText('Test title')).toBeInTheDocument();
+    expect(container.querySelector('#mocked-field-field1')).toBeInTheDocument();
+    expect(container.querySelector('#mocked-field-field2')).toBeInTheDocument();
+    expect(
+      container.querySelector('#mocked-field-field3'),
+    ).not.toBeInTheDocument();
+    expect(
+      container.querySelector('#mocked-field-field4'),
+    ).not.toBeInTheDocument();
+
+    fireEvent.click(container.querySelector('button[aria-label="Submit"]'));
+    fireEvent.click(container.querySelector('button[aria-label="Cancel"]'));
+  });
+
+  it('renders without crashing and selecting Submit/Cancel button with no errors', () => {
+    config.blocks = {
+      initialBlocksFocus: null,
+    };
+    config.settings = {
+      verticalFormTabs: true,
+    };
+
+    const props = {
+      schema: {
+        fieldsets: [
+          {
+            fields: ['field1', 'field2'],
+            id: 'fieldset1',
+            title: 'Fieldset 1',
+          },
+          {
+            fields: ['field3', 'field4'],
+            id: 'fieldset2',
+            title: 'Fieldset 2',
+          },
+        ],
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { widget: 'textarea' },
+        },
         definitions: {},
         required: [],
       },
@@ -368,6 +589,12 @@ describe('Form', () => {
       onCancel: jest.fn(),
       submitLabel: 'Submit',
     };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => ({}));
 
     const { container, getByText } = render(
       <Provider store={store}>
@@ -468,6 +695,430 @@ describe('Form', () => {
     fireEvent.blur(container.querySelector('#mocked-field-field2 textarea'));
     fireEvent.change(container.querySelector('#mocked-field-field2 textarea'), {
       target: { value: 'test change' },
+    });
+  });
+
+  it('triggers the onSubmit with the shiftKey and multiple selected blocks and multiple erorrs', () => {
+    config.blocks = {
+      initialBlocksFocus: null,
+    };
+    config.settings = {
+      verticalFormTabs: true,
+    };
+
+    const props = {
+      schema: {
+        fieldsets: [
+          {
+            fields: ['field1', 'field2'],
+            id: 'fieldset1',
+            title: 'Fieldset 1',
+          },
+        ],
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { title: 'Field 2', widget: 'textarea' },
+        },
+        definitions: {},
+        required: [],
+      },
+      formData: {
+        blocks: {
+          id1: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+        },
+        blocks_layout: {
+          items: ['id1'],
+        },
+      },
+      type: 'typeB',
+      isClient: true,
+      visual: true,
+      title: 'Test title',
+      description: 'Test description',
+      error: {
+        message: 'Sample error message',
+      },
+      onSubmit: jest.fn(),
+      onCancel: jest.fn(),
+    };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => [
+      'field1',
+      'field2',
+    ]);
+
+    const { container } = render(
+      <Provider store={store}>
+        <Form {...props} />
+      </Provider>,
+    );
+
+    fireEvent.click(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'id1,id2' },
+    });
+    fireEvent.change(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'test change' },
+    });
+    fireEvent.select(container.querySelector('#blocks-toolbar'), {
+      target: {
+        id: 'id1',
+        isMultipleSelection: true,
+        event: { shiftKey: true },
+      },
+    });
+
+    fireEvent.change(container.querySelector('#blocks-form'), {
+      target: { value: 'test change' },
+    });
+  });
+
+  it('triggers the onSubmit with the shiftKey and multiple selected blocks when the selected blocks are in another order and multiple errors', () => {
+    config.blocks = {
+      initialBlocksFocus: null,
+    };
+    config.settings = {
+      verticalFormTabs: true,
+    };
+
+    const props = {
+      schema: {
+        fieldsets: [
+          {
+            fields: ['field1', 'field2'],
+            id: 'fieldset1',
+            title: 'Fieldset 1',
+          },
+        ],
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { title: 'Field 2', widget: 'textarea' },
+        },
+        definitions: {},
+        required: [],
+      },
+      formData: {
+        blocks: {
+          id1: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+          id2: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+        },
+        blocks_layout: {
+          items: ['id1', 'id2'],
+        },
+      },
+      type: 'typeB',
+      isClient: true,
+      visual: true,
+      title: 'Test title',
+      description: 'Test description',
+      error: {
+        message: 'Sample error message',
+      },
+      onSubmit: jest.fn(),
+      onCancel: jest.fn(),
+    };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => [
+      'field1',
+      'field2',
+    ]);
+
+    const { container } = render(
+      <Provider store={store}>
+        <Form {...props} />
+      </Provider>,
+    );
+
+    fireEvent.click(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'id2,id1' },
+    });
+    fireEvent.change(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'test change' },
+    });
+    fireEvent.select(container.querySelector('#blocks-toolbar'), {
+      target: {
+        id: 'id1',
+        isMultipleSelection: true,
+        event: { shiftKey: true },
+      },
+    });
+  });
+
+  it('triggers the onSubmit with the shiftKey and multiple selected blocks when there are no selected blocks and multiple errors', () => {
+    config.blocks = {
+      initialBlocksFocus: null,
+    };
+    config.settings = {
+      verticalFormTabs: true,
+    };
+
+    const props = {
+      schema: {
+        fieldsets: [
+          {
+            fields: ['field1', 'field2'],
+            id: 'fieldset1',
+            title: 'Fieldset 1',
+          },
+        ],
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { title: 'Field 2', widget: 'textarea' },
+        },
+        definitions: {},
+        required: [],
+      },
+      formData: {
+        blocks: {
+          id1: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+        },
+        blocks_layout: {
+          items: ['id1'],
+        },
+      },
+      type: 'typeB',
+      isClient: true,
+      visual: true,
+      title: 'Test title',
+      description: 'Test description',
+      error: {
+        message: 'Sample error message',
+      },
+      onSubmit: jest.fn(),
+      onCancel: jest.fn(),
+    };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => [
+      'field1',
+      'field2',
+    ]);
+
+    const { container } = render(
+      <Provider store={store}>
+        <Form {...props} />
+      </Provider>,
+    );
+
+    fireEvent.click(container.querySelector('#blocks-toolbar'), {
+      target: { value: undefined },
+    });
+    fireEvent.change(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'test change' },
+    });
+    fireEvent.select(container.querySelector('#blocks-toolbar'), {
+      target: {
+        id: 'id1',
+        isMultipleSelection: true,
+        event: { shiftKey: true },
+      },
+    });
+  });
+
+  it('triggers the onSubmit with the ctrlKey and multiple selected blocks and multiple errors', () => {
+    config.blocks = {
+      initialBlocksFocus: null,
+    };
+    config.settings = {
+      verticalFormTabs: true,
+    };
+
+    const props = {
+      schema: {
+        fieldsets: [
+          {
+            fields: ['field1', 'field2'],
+            id: 'fieldset1',
+            title: 'Fieldset 1',
+          },
+        ],
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { title: 'Field 2', widget: 'textarea' },
+        },
+        definitions: {},
+        required: [],
+      },
+      formData: {
+        blocks: {
+          id1: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+        },
+        blocks_layout: {
+          items: ['id1'],
+        },
+      },
+      type: 'typeB',
+      isClient: true,
+      visual: true,
+      title: 'Test title',
+      description: 'Test description',
+      error: {
+        message: 'Sample error message',
+      },
+      onSubmit: jest.fn(),
+      onCancel: jest.fn(),
+    };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => [
+      'field1',
+      'field2',
+    ]);
+
+    const { container } = render(
+      <Provider store={store}>
+        <Form {...props} />
+      </Provider>,
+    );
+
+    fireEvent.click(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'id1,id2' },
+    });
+    fireEvent.change(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'test change' },
+    });
+    fireEvent.select(container.querySelector('#blocks-toolbar'), {
+      target: {
+        id: 'id1',
+        isMultipleSelection: true,
+        event: { ctrlKey: true },
+      },
+    });
+  });
+
+  it('triggers the onSubmit with the ctrlKey and multiple selected blocks and multiple errors when the selected block in not in the list of selected blocks', () => {
+    config.blocks = {
+      initialBlocksFocus: null,
+    };
+    config.settings = {
+      verticalFormTabs: true,
+    };
+
+    const props = {
+      schema: {
+        fieldsets: [
+          {
+            fields: ['field1', 'field2'],
+            id: 'fieldset1',
+            title: 'Fieldset 1',
+          },
+        ],
+        properties: {
+          field1: {
+            title: 'Field 1',
+            description: 'Field 1 description',
+            items: ['field4'],
+            widget: 'textarea',
+          },
+          field2: { title: 'Field 2', widget: 'textarea' },
+        },
+        definitions: {},
+        required: [],
+      },
+      formData: {
+        blocks: {
+          id1: {
+            '@type': 'typeB',
+            plaintext: 'Block A',
+            override_toc: false,
+          },
+        },
+        blocks_layout: {
+          items: ['id1'],
+        },
+      },
+      type: 'typeB',
+      isClient: true,
+      visual: true,
+      title: 'Test title',
+      description: 'Test description',
+      error: {
+        message: 'Sample error message',
+      },
+      onSubmit: jest.fn(),
+      onCancel: jest.fn(),
+    };
+
+    const validateFieldsPerFieldsetMock = jest.spyOn(
+      FormValidation,
+      'validateFieldsPerFieldset',
+    );
+    validateFieldsPerFieldsetMock.mockImplementation(() => [
+      'field1',
+      'field2',
+    ]);
+
+    const { container } = render(
+      <Provider store={store}>
+        <Form {...props} />
+      </Provider>,
+    );
+
+    fireEvent.click(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'id1,id2' },
+    });
+    fireEvent.change(container.querySelector('#blocks-toolbar'), {
+      target: { value: 'test change' },
+    });
+    fireEvent.select(container.querySelector('#blocks-toolbar'), {
+      target: {
+        id: 'id3',
+        isMultipleSelection: true,
+        event: { ctrlKey: true },
+      },
     });
   });
 });
