252155a10dfbaaec73a10265641f8d66f22bc73a
dana-cfc4 <dana.comiselu4@gmail.com>
2023-08-11 13:32:26 +0300

    change: add filter icons into a single object

--- src/index.js
+++ src/index.js
@@ -84,8 +84,10 @@ const applyConfig = (config) => {
   // Apply accordion block customization
   if (config.blocks.blocksConfig.accordion) {
     config.blocks.blocksConfig.accordion.semanticIcon = 'ri-arrow-down-s-line';
-    config.blocks.blocksConfig.accordion.filterIcon = 'ri-filter-3-line';
-    config.blocks.blocksConfig.accordion.closeIcon = 'ri-close-line';
+    config.blocks.blocksConfig.accordion.filterIcon = {
+      opened: 'accordion-icon-opened',
+      closed: 'accordion-icon-closed',
+    };
     config.blocks.blocksConfig.accordion.options = {};
     config.blocks.blocksConfig.accordion.defaults.theme = 'secondary';
   }
