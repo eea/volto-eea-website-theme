6a3f261a1ff11245f40899cf8f75420077b993a6
kreafox <krisztina.n.elekes@gmail.com>
2023-08-17 15:09:10 +0300

    fix(tests): try fixing failing test on cypress

--- src/components/theme/BaseTag.jsx
+++ src/components/theme/BaseTag.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Helmet } from '@plone/volto/helpers';
 
 export default function BaseTag(props) {
-  const contentId = useSelector((store) => store.content.data?.['@id']);
+  const contentId = useSelector((store) => store?.content?.data?.['@id']);
   return contentId ? (
     <Helmet base={{ target: '_blank', href: `${contentId}/` }} />
   ) : null;
