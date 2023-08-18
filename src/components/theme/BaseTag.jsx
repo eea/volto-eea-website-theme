248e9534364e550b8b0770f3f336c98e73cad434
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-08-18 21:21:11 +0300

    Cleanup

--- src/components/theme/BaseTag.jsx
+++ src/components/theme/BaseTag.jsx
@@ -3,9 +3,7 @@ import { flattenToAppURL, Helmet } from '@plone/volto/helpers';
 
 export default function BaseTag(props) {
   const contentId = useSelector((store) => store?.content?.data?.['@id']);
-  // const isCypress = typeof window !== 'undefined' && !window.__Cypress__;
   const baseHref = contentId ? flattenToAppURL(contentId) : null;
-  // console.log('set baseHref', baseHref); // __CLIENT__ &&
 
   return baseHref !== null ? (
     <Helmet base={{ target: '_blank', href: `${baseHref}/` }} />
