361d0269c5f2ec5bad5e12727903a89aa3d1607d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-08-18 21:10:44 +0300

    Maybe now

--- src/components/theme/BaseTag.jsx
+++ src/components/theme/BaseTag.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux';
-import Helmet from '@plone/volto/helpers/Helmet/Helmet';
+import { flattenToAppURL, Helmet } from '@plone/volto/helpers';
 
 export default function BaseTag(props) {
   const contentId = useSelector((store) => store?.content?.data?.['@id']);
-  const isCypress = typeof window !== 'undefined' && !window.__Cypress__;
+  // const isCypress = typeof window !== 'undefined' && !window.__Cypress__;
+  const baseHref = contentId ? flattenToAppURL(contentId) : null;
+  // console.log('set baseHref', baseHref); // __CLIENT__ &&
 
-  return contentId && !isCypress ? (
-    <Helmet base={{ target: '_blank', href: `${contentId}/` }} />
+  return baseHref !== null ? (
+    <Helmet base={{ target: '_blank', href: `${baseHref}/` }} />
   ) : null;
 }
