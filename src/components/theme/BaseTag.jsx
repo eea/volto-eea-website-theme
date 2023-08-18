64361fefa5b2136c6b91b2f7e55ff56b5bc5bf15
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-08-18 17:32:30 +0300

    Protect against cypress

--- src/components/theme/BaseTag.jsx
+++ src/components/theme/BaseTag.jsx
@@ -1,12 +1,11 @@
 import { useSelector } from 'react-redux';
 import Helmet from '@plone/volto/helpers/Helmet/Helmet';
 
-// const Helmet = () => null;
-
 export default function BaseTag(props) {
   const contentId = useSelector((store) => store?.content?.data?.['@id']);
-  // base={{ target: '_blank', href: `${contentId}/` }}
-  return contentId && __CLIENT__ ? (
-    <Helmet base={{ target: '_blank' }} />
+  const isCypress = typeof window !== 'undefined' && !window.__Cypress__;
+
+  return contentId && !isCypress ? (
+    <Helmet base={{ target: '_blank', href: `${contentId}/` }} />
   ) : null;
 }
