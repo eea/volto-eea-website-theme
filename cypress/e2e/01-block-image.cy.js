9ab7cfc0e0e5ff7e79beffdcf9211faf17cede52
nileshgulia1 <nileshgulia@gmail.com>
2023-09-14 14:34:58 +0530

    chore: add cypress for image-block"

---
+++ cypress/e2e/01-block-image.cy.js
@@ -0,0 +1,57 @@
+import { slateBeforeEach, slateAfterEach } from '../support/e2e';
+
+describe('Blocks Tests', () => {
+  beforeEach(slateBeforeEach);
+  afterEach(slateAfterEach);
+
+  it('Add Block: Image', () => {
+    // Change page title
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('My Add-on Page');
+
+    cy.get('.documentFirstHeading').contains('My Add-on Page');
+
+    cy.getSlate().click();
+
+    // test rss link
+    cy.get('.documentFirstHeading').click();
+    cy.get('a').contains('Block').click();
+    cy.get(`[aria-label="Add RSS Link"]`).click();
+    cy.get('#field-title-0-rssLinks-0').type('Test EEA Theme');
+    cy.get('#field-href-2-rssLinks-0').type('test.com');
+
+    //add a block
+    cy.getSlate().click();
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Media').click();
+    cy.get('.content.active.media .button.image').contains('Image').click();
+
+    cy.get('.block.image .ui.input input[type="text"]').type(
+      `https://github.com/plone/volto/raw/master/logos/volto-colorful.png{enter}`,
+    );
+    // then the page view should contain the image block
+
+    // check banner rss link
+    cy.get('.button.rssfeed').click();
+
+    // Save
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+
+    cy.get('#page-document img').should(
+      'have.attr',
+      'src',
+      'https://github.com/plone/volto/raw/master/logos/volto-colorful.png',
+    );
+
+    cy.get('#page-document img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+
+    // then the page view should contain our changes
+    cy.contains('My Add-on Page');
+  });
+});
