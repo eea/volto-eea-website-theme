d29808a10fe8daca2f023c902adeb756ea61828a
Alin Voinea <contact@avoinea.com>
2023-10-31 15:40:10 +0200

    docs: Add info about this normalizeExternalData.js shadow - refs #259857

--- src/customizations/@plone/volto-slate/editor/extensions/normalizeExternalData.js
+++ src/customizations/@plone/volto-slate/editor/extensions/normalizeExternalData.js
@@ -1,3 +1,4 @@
+// To be removed when https://github.com/plone/volto/pull/5347 is merged and released in Volto 16.x
 import { normalizeExternalData as normalize } from '@plone/volto-slate/utils';
 
 export function normalizeExternalData(editor) {
