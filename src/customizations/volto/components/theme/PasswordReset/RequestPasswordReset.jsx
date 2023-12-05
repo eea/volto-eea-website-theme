957c6635397d231e7984f28a6eacf5aaa0327889
ana-oprea <anaop3212@gmail.com>
2023-12-05 12:02:08 +0200

    chore: remove style.less -refs #259024

--- src/customizations/volto/components/theme/PasswordReset/RequestPasswordReset.jsx
+++ src/customizations/volto/components/theme/PasswordReset/RequestPasswordReset.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { FormattedMessage } from 'react-intl';
-import './style.less';
 
 function RequestPasswordReset() {
   return (
