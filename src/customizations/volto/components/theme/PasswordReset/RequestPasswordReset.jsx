232866f07be97bafd924991e192ea63894491a17
ana-oprea <anaop3212@gmail.com>
2023-12-05 11:11:00 +0200

    feat: modify the password reset page -refs #259024

---
+++ src/customizations/volto/components/theme/PasswordReset/RequestPasswordReset.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import { FormattedMessage } from 'react-intl';
+import './style.less';
+
+// function RequestPasswordReset() {
+//   return (
+//     <>
+//       <h1 className="documentFirstHeading">Lost Password</h1>
+//       <div className="documentDescription">
+//         For security reasons, we store your password encrypted, and cannot mail
+//         it to you. If you would like to reset your password, fill out the form
+//         below and we will send you an email at the address you gave when you
+//         registered to start the process of resetting your password.
+//       </div>
+//       <p id="content-core">
+//         To reset your password visit{' '}
+//         <a href="https://www.eionet.europa.eu/password-reset">
+//           Reset Eionet account password page
+//         </a>
+//         .
+//       </p>
+//     </>
+//   );
+// }
+
+function RequestPasswordReset() {
+  return (
+    <Container id="passwordreset">
+      <h1 className="documentFirstHeading">
+        <FormattedMessage id="Lost Password" defaultMessage="Lost Password" />
+      </h1>
+      <p className="documentDescription">
+        <div>
+          <FormattedMessage
+            id="For security reasons, we store your password encrypted, and cannot mail it to you. If you would like to reset your password, fill out the form below and we will send you an email at the address you gave when you registered to start the process of resetting your password."
+            defaultMessage="For security reasons, we store your password encrypted, and cannot mail it to you. If you would like to reset your password, fill out the form below and we will send you an email at the address you gave when you registered to start the process of resetting your password."
+          />
+        </div>
+      </p>
+      <div className="documentContent">
+        <p>
+          <FormattedMessage
+            id="To reset your password visit {link}."
+            defaultMessage="To reset your password visit {link}."
+            values={{
+              link: (
+                <a href="https://www.eionet.europa.eu/password-reset">
+                  <FormattedMessage
+                    id="Reset Eionet account password page"
+                    defaultMessage="Reset Eionet account password page"
+                  />
+                </a>
+              ),
+            }}
+          />
+        </p>
+      </div>
+    </Container>
+  );
+}
+
+export default RequestPasswordReset;
