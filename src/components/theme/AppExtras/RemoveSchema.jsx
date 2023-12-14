1f432cff747ae128e3743eeb484ff3812ecbb3a9
Miu Razvan <miu.razvan28@gmail.com>
2023-12-14 15:58:06 +0200

    feat: add bg color to status

--- src/components/theme/AppExtras/RemoveSchema.jsx
+++ src/components/theme/AppExtras/RemoveSchema.jsx
@@ -1,16 +1,20 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { removeSchema } from '@eeacms/volto-eea-website-theme/actions';
+import { flattenToAppURL } from '@plone/volto/helpers';
 
-function RemoveSchema({ removeSchema, content, schema }) {
+function RemoveSchema({ removeSchema, contentLoading, content, schema }) {
   useEffect(() => {
     if (
+      !schema.loading &&
+      !contentLoading &&
       schema.schema &&
-      (!content || (content && schema.contentType !== content['@type']))
+      (!content ||
+        (content && schema.contentUrl !== flattenToAppURL(content['@id'])))
     ) {
       removeSchema();
     }
-  }, [removeSchema, content, schema]);
+  }, [removeSchema, content, contentLoading, schema]);
 
   return null;
 }
@@ -18,6 +22,7 @@ function RemoveSchema({ removeSchema, content, schema }) {
 export default connect(
   (state) => ({
     content: state.content.data,
+    contentLoading: state.content.get.loading,
     schema: state.schema,
   }),
   { removeSchema },
--- src/middleware/apiEnhancer.js
+++ src/middleware/apiEnhancer.js
@@ -1,4 +1,4 @@
-export default function apiEnhencar(middlewares) {
+export default function apiEnhencer(middlewares) {
   return [
     () => (next) => (action) => {
       if (action.request) {
