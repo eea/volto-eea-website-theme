53d7f26b9dcfcc17e1b0104a1d1d77f6bcc5e4d9
David Ichim <ichim.david@gmail.com>
2024-02-06 10:37:43 +0200

    Fixed getCurrentPage results when using 2 listing blocks
    - Volto logic for pagination assumes the pagination is present when there is more that one block

--- src/customizations/volto/components/manage/Blocks/Listing/getAsyncData.js
+++ src/customizations/volto/components/manage/Blocks/Listing/getAsyncData.js
@@ -1,17 +1,28 @@
 import { getQueryStringResults } from '@plone/volto/actions';
 import { resolveBlockExtensions } from '@plone/volto/helpers';
+import qs from 'query-string';
+import { slugify } from '@plone/volto/helpers/Utils/Utils';
 
-export default function getListingBlockAsyncData({
-  dispatch,
-  data,
-  path,
-  blocksConfig,
-  content,
-}) {
+const getCurrentPage = (location, id) => {
+  const pageQueryParam = qs.parse(location.search);
+  switch (Object.keys(pageQueryParam).length) {
+    case 0:
+      return 1;
+    case 1:
+      // when there is only one query param, it could be the simple page number or the sluggified block id
+      return pageQueryParam['page'] || pageQueryParam[slugify(`page-${id}`)];
+    default:
+      return pageQueryParam[slugify(`page-${id}`)];
+  }
+};
+
+export default function getListingBlockAsyncData(props) {
+  const { data, path, location, id, dispatch, blocksConfig, content } = props;
+
   const { resolvedExtensions } = resolveBlockExtensions(data, blocksConfig);
-  const id = data.block;
 
   const subrequestID = content?.UID ? `${content?.UID}-${id}` : id;
+  const currentPage = getCurrentPage(location, id);
 
   return [
     dispatch(
@@ -24,6 +35,7 @@ export default function getListingBlockAsyncData({
             : { metadata_fields: '_all' }),
         },
         subrequestID,
+        currentPage,
       ),
     ),
   ];
