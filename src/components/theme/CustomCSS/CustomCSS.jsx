0f41cb59a37370ccada7bc447c272e00caa8f05e
David Ichim <ichim.david@gmail.com>
2024-05-22 18:08:55 +0300

    fix(perf): preload voltoCustom.css in order to avoid layout shift

--- src/components/theme/CustomCSS/CustomCSS.jsx
+++ src/components/theme/CustomCSS/CustomCSS.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import config from '@plone/volto/registry';
 
 const CustomCSS = (props) => {
+  const href = `${config.settings.apiPath}/voltoCustom.css`;
   return (
-    <link
-      rel={'stylesheet'}
-      href={`${config.settings.apiPath}/voltoCustom.css`}
-    />
+    <>
+      <link
+        rel="preload"
+        href={href}
+        as="style"
+        onload="this.onload=null;this.rel='stylesheet'"
+      />
+      <noscript>
+        <link rel="stylesheet" href={href} />
+      </noscript>
+    </>
   );
 };
 export default CustomCSS;
