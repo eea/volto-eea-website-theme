576ec49a98b46f77a75d1b3e619868ce12427f72
David Ichim <ichim.david@gmail.com>
2024-05-23 19:44:08 +0300

    revert lazy loading of voltoCustom.css as it didn't properly parse it as stylesheet

--- src/components/theme/CustomCSS/CustomCSS.jsx
+++ src/components/theme/CustomCSS/CustomCSS.jsx
@@ -5,15 +5,7 @@ const CustomCSS = (props) => {
   const href = `${config.settings.apiPath}/voltoCustom.css`;
   return (
     <>
-      <link
-        rel="preload"
-        href={href}
-        as="style"
-        onload="this.onload=null;this.rel='stylesheet'"
-      />
-      <noscript>
-        <link rel="stylesheet" href={href} />
-      </noscript>
+      <link rel="stylesheet" href={href} />
     </>
   );
 };
