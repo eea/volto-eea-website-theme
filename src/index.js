956141cbeafd84938742b920ba40ad599eec1b19
Tripon Eugen <tripon.eugen@gmail.com>
2024-06-07 14:14:05 +0300

    Refs #269520 - import TokenWidgetEdit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { TopicsWidget } from '@eeacms/volto-eea-website-theme/components/theme/W
 import CreatableSelectWidget from '@eeacms/volto-eea-website-theme/components/theme/Widgets/CreatableSelectWidget';
 
 import { Icon } from '@plone/volto/components';
-import { TokenWidget as TokenWidgetEdit } from '@plone/volto/components';
+import { default as TokenWidgetEdit } from '@plone/volto/components/manage/Widgets/TokenWidget';
 import { serializeNodesToText } from '@plone/volto-slate/editor/render';
 import Tag from '@eeacms/volto-eea-design-system/ui/Tag/Tag';
 
