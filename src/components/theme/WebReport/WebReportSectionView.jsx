0aa7010fec238a739a80fe7e5f56e19e712ab4be
nileshgulia1 <nileshgulia@gmail.com>
2024-10-02 11:57:07 +0530

    fix react import

--- src/components/theme/WebReport/WebReportSectionView.jsx
+++ src/components/theme/WebReport/WebReportSectionView.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { isInternalURL, flattenToAppURL } from '@plone/volto/helpers';
