d5282b3a0c4d98a4b905bc6ce2f569a5bab3bd1b
David Ichim <ichim.david@gmail.com>
2024-10-18 16:10:41 +0300

    bump bottomLevel depth to 4 in order to get 4 levels of own content for context navigation

--- src/customizations/volto/components/theme/View/DefaultView.jsx
+++ src/customizations/volto/components/theme/View/DefaultView.jsx
@@ -32,8 +32,10 @@ import AccordionContextNavigation from '@eeacms/volto-eea-website-theme/componen
 const DefaultView = (props) => {
   const { content, location } = props;
   const [hasLightLayout, setHasLightLayout] = React.useState(false);
+  console.count('DefaultView');
 
   React.useEffect(() => {
+    console.count('useEffect');
     const updateLightLayout = () => {
       if (__CLIENT__) {
         setHasLightLayout(document.body.classList.contains('light-header'));
@@ -43,6 +45,7 @@ const DefaultView = (props) => {
     updateLightLayout();
 
     if (__CLIENT__) {
+      console.count('if __CLIENT__');
       const observer = new MutationObserver(updateLightLayout);
       observer.observe(document.body, {
         attributes: true,
@@ -113,7 +116,7 @@ const DefaultView = (props) => {
               no_icons: matchingNavigationPath.no_icons || true,
               root_path: matchingNavigationPath.url,
               includeTop: matchingNavigationPath.includeTop || true,
-              bottomLevel: matchingNavigationPath.bottomLevel || 3,
+              bottomLevel: matchingNavigationPath.bottomLevel || 4,
               topLevel: matchingNavigationPath.topLevel || 1,
               currentFolderOnly:
                 matchingNavigationPath.currentFolderOnly || false,
