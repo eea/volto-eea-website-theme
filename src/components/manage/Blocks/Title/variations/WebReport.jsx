d3c7f8e6cafb7d57b7e59a65fa28dd540003ba1e
David Ichim <ichim.david@gmail.com>
2024-10-28 17:47:19 +0200

    fix(title-block): above title content for Web report variation when we hide content type
    - previous code allowed the duplication of subtitle due to aboveTitle being false

--- src/components/manage/Blocks/Title/variations/WebReport.jsx
+++ src/components/manage/Blocks/Title/variations/WebReport.jsx
@@ -38,10 +38,12 @@ const WebReport = (props) => {
         {...props}
         data={{
           ...props.data,
-          aboveTitle: !props.data.hideContentType && (
+          aboveTitle: !props.data.hideContentType ? (
             <div className="content-type">
               {props.data.content_type || props.properties.type_title}
             </div>
+          ) : (
+            ' '
           ),
           belowTitle: (
             <>
