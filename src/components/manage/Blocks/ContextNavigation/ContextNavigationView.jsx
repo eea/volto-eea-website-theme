48fdf6089e21aeefacf927310b9a41ea9cd0e2be
David Ichim <ichim.david@gmail.com>
2024-12-05 21:44:59 +0200

    fix(ContextNavigation): Add memoization for View that triggered fetch on Edit page modifications

--- src/components/manage/Blocks/ContextNavigation/ContextNavigationView.jsx
+++ src/components/manage/Blocks/ContextNavigation/ContextNavigationView.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import { flattenToAppURL, withBlockExtensions } from '@plone/volto/helpers';
 import DefaultTemplate from './variations/Default';
 
-const ContextNavigationView = (props = {}) => {
-  const { variation, data = {} } = props;
-  const navProps = { ...data };
-  const root_path = data?.root_node?.[0]?.['@id'];
-  if (root_path) navProps['root_path'] = flattenToAppURL(root_path);
-  const Renderer = variation?.view ?? DefaultTemplate;
-  return <Renderer params={navProps} mode={props.mode} />;
-};
+const ContextNavigationView = React.memo(
+  (props = {}) => {
+    const { variation, data = {} } = props;
+    const navProps = React.useMemo(() => {
+      const props = { ...data };
+      const root_path = data?.root_node?.[0]?.['@id'];
+      if (root_path) props['root_path'] = flattenToAppURL(root_path);
+      return props;
+    }, [data]);
+
+    const Renderer = variation?.view ?? DefaultTemplate;
+    return <Renderer params={navProps} mode={props.mode} />;
+  },
+  (prevProps, nextProps) => {
+    // check if props have changed for memo
+    return (
+      prevProps.mode === nextProps.mode &&
+      prevProps.variation === nextProps.variation &&
+      JSON.stringify(prevProps.data) === JSON.stringify(nextProps.data)
+    );
+  },
+);
 
 export default withBlockExtensions(ContextNavigationView);
