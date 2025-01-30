04a3d37eb3e0e9d13f3f7c4ef866189577b2ec8c
alin <contact@avoinea.com>
2025-01-30 21:07:41 +0200

    test: Fix cypress for removed maps block

--- cypress/e2e/03-blocks-copypaste.cy.js
+++ cypress/e2e/03-blocks-copypaste.cy.js
@@ -6,11 +6,11 @@ describe('Blocks copy/paste', () => {
 
   it('Copy/paste multiple blocks', () => {
     // GIVEN: A page with multiple blocks
-    cy.getSlate().click().type('/maps{enter}');
+    cy.getSlate().click().type('/image{enter}');
 
-    cy.get(`.block.maps .toolbar-inner .ui.input input`)
+    cy.get(`.block.image .toolbar-inner .ui.input input`)
       .type(
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2525.497070288158!2d7.103133415464086!3d50.72926897951482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bee17434076fc7%3A0x2e99668f581378c8!2sRiesstra%C3%9Fe+21%2C+53113+Bonn!5e0!3m2!1sde!2sde!4v1561386702097!5m2!1sde!2sde" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+        'https://eea.github.io/volto-eea-design-system/img/eea_icon.png',
       )
       .type('{enter}');
 
@@ -22,7 +22,7 @@ describe('Blocks copy/paste', () => {
 
     // WHEN: I copy paste them
     cy.getSlateTitle().focus().click().type('{shift}', { release: false });
-    cy.get('.block-editor-maps').click();
+    cy.get('.block-editor-image').click();
     cy.get('#toolbar-copy-blocks').click();
 
     cy.getSlateEditorAndType('{shift}').click();
@@ -34,7 +34,7 @@ describe('Blocks copy/paste', () => {
       expect($blocks).to.have.length(2);
     });
 
-    cy.get('.block-editor-maps').should(($blocks) => {
+    cy.get('.block-editor-image').should(($blocks) => {
       expect($blocks).to.have.length(2);
     });
   });
@@ -42,10 +42,10 @@ describe('Blocks copy/paste', () => {
   it('Cut/paste multiple blocks', () => {
     // GIVEN: A page with multiple blocks
 
-    cy.getSlate().click().type('/maps{enter}');
-    cy.get(`.block.maps .toolbar-inner .ui.input input`)
+    cy.getSlate().click().type('/image{enter}');
+    cy.get(`.block.image .toolbar-inner .ui.input input`)
       .type(
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2525.497070288158!2d7.103133415464086!3d50.72926897951482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bee17434076fc7%3A0x2e99668f581378c8!2sRiesstra%C3%9Fe+21%2C+53113+Bonn!5e0!3m2!1sde!2sde!4v1561386702097!5m2!1sde!2sde" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+        'https://eea.github.io/volto-eea-design-system/img/eea_icon.png',
       )
       .type('{enter}');
 
@@ -56,7 +56,7 @@ describe('Blocks copy/paste', () => {
     cy.getSlate().click();
 
     cy.getSlateTitle().focus().click().type('{shift}', { release: false });
-    cy.get('.block-editor-maps').click();
+    cy.get('.block-editor-image').click();
     cy.get('#toolbar-cut-blocks').click();
 
     cy.getSlateEditorAndType('{shift}').click();
@@ -69,17 +69,17 @@ describe('Blocks copy/paste', () => {
       expect($blocks).to.have.length(1);
     });
 
-    cy.get('.block-editor-maps').should(($blocks) => {
+    cy.get('.block-editor-image').should(($blocks) => {
       expect($blocks).to.have.length(1);
     });
   });
 
   it('Delete multiple blocks', () => {
     // GIVEN: A page with multiple blocks
-    cy.getSlate().click().type('/maps{enter}');
-    cy.get(`.block.maps .toolbar-inner .ui.input input`)
+    cy.getSlate().click().type('/image{enter}');
+    cy.get(`.block.image .toolbar-inner .ui.input input`)
       .type(
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2525.497070288158!2d7.103133415464086!3d50.72926897951482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bee17434076fc7%3A0x2e99668f581378c8!2sRiesstra%C3%9Fe+21%2C+53113+Bonn!5e0!3m2!1sde!2sde!4v1561386702097!5m2!1sde!2sde" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+        'https://eea.github.io/volto-eea-design-system/img/eea_icon.png',
       )
       .type('{enter}');
 
@@ -90,7 +90,7 @@ describe('Blocks copy/paste', () => {
     cy.getSlate().click();
 
     cy.getSlateTitle().focus().type('{shift}', { release: false });
-    cy.get('.block-editor-maps').click();
+    cy.get('.block-editor-image').click();
     cy.get('#toolbar-delete-blocks').should('be.visible');
     cy.get('#toolbar-delete-blocks').click();
 
@@ -99,7 +99,7 @@ describe('Blocks copy/paste', () => {
       expect($blocks).to.have.length(0);
     });
 
-    cy.get('.block-editor-maps').should(($blocks) => {
+    cy.get('.block-editor-image').should(($blocks) => {
       expect($blocks).to.have.length(0);
     });
   });
