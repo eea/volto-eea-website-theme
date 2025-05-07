139f614658ef431f9b347a851d2ad6c015d5031e
David Ichim <ichim.david@gmail.com>
2025-05-07 20:38:04 +0300

    change(item): added one column for flex-group refs #279420

--- src/index.js
+++ src/index.js
@@ -338,6 +338,7 @@ const applyConfig = (config) => {
             title: 'No. of columns',
             description: 'Choose the number of flex columns',
             choices: [
+              [1, 1],
               [2, 2],
               [3, 3],
               [4, 4],
