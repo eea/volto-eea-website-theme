3d180ec365ee7d4b7a96f8ad3cdae532b4834ec3
David Ichim <ichim.david@gmail.com>
2025-09-25 22:35:02 +0300

    change(custom-js): removed App.jsx override in favor of adding to CustomCSS.jsx

--- src/components/theme/CustomCSS/CustomCSS.jsx
+++ src/components/theme/CustomCSS/CustomCSS.jsx
@@ -3,9 +3,11 @@ import config from '@plone/volto/registry';
 
 const CustomCSS = (props) => {
   const href = `${config.settings.apiPath}/voltoCustom.css`;
+  const src = `${config.settings.apiPath}/voltoCustom.js`;
   return (
     <>
       <link rel="stylesheet" href={href} />
+      <script type="text/javascript" src={src}></script>
     </>
   );
 };
