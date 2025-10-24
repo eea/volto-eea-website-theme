aa848866b931878c8fd276cbef11dc3701a3f122
nileshgulia1 <nileshgulia@gmail.com>
2025-10-24 22:15:31 +0530

    fix tests

--- src/hocs/withErrorBoundary.test.jsx
+++ src/hocs/withErrorBoundary.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { ErrorBoundary } from '@plone/volto/components';
 import withErrorBoundary from './withErrorBoundary';
 
