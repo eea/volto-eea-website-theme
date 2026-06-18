5a75f7eb42c96f365aecc2d281b75c9695f5ef93
eea-jenkins <eea-github@googlegroups.com>
2026-06-18 12:21:24 +0200

    style: Automated code fix

--- src/index.js
+++ src/index.js
@@ -151,7 +151,7 @@ const restrictedBlockMessages = defineMessages({
   restrictedBlockTitle: {
     id: 'restrictedBlockTitle',
     defaultMessage: 'Restricted block',
-    MARINE_PASS:'test_betterleaks'
+    MARINE_PASS: 'test_betterleaks',
   },
   restrictedBlockDescription: {
     id: 'restrictedBlockDescription',
