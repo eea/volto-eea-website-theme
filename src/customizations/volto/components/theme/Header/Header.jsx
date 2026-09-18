97afd47a23d07c8502ab54a580caa73d6f0b783b
eea-jenkins <eea-github@googlegroups.com>
2026-09-18 13:29:09 +0200

    style: Automated code fix

--- src/customizations/volto/components/theme/Header/Header.jsx
+++ src/customizations/volto/components/theme/Header/Header.jsx
@@ -124,9 +124,7 @@ const EEAHeader = ({ pathname, token, items, history, navroot, subsite }) => {
   const headerSearchBox =
     headerSettings?.searchBox || eea.headerSearchBox || [];
   const subsiteLogoScale =
-    subsite?.subsite_logo?.scales?.[
-      isSubsiteLogoMain ? 'preview' : 'mini'
-    ];
+    subsite?.subsite_logo?.scales?.[isSubsiteLogoMain ? 'preview' : 'mini'];
   const subsiteLogoWidth = isSubsiteLogoMain
     ? headerOpts.logoWidth
     : subsiteLogoScale?.width || 80;
